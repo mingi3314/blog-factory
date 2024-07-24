0152597d407fa65dd8878d690d1945d690492cef
Minki Kim <mingi3314@gmail.com>
2024-07-24 18:04:21 +0900

    format codes

--- scripts/initialize_images.py
+++ scripts/initialize_images.py
@@ -3,25 +3,28 @@ from pathlib import Path
 
 import requests
 
+
 def generate_random_logo(seed: str, file_path: Path) -> None:
     """Generate a random logo and save it to the specified path."""
     url = f"https://robohash.org/{seed}.png"
     response = requests.get(url)
     response.raise_for_status()
-    
+
     file_path.write_bytes(response.content)
     print(f"Logo saved to {file_path}")
+
 
 def generate_random_thumbnail(width: int, height: int, file_path: Path) -> None:
     """Generate a random thumbnail and save it to the specified path."""
     url = f"https://picsum.photos/{width}/{height}"
     response = requests.get(url)
     response.raise_for_status()
-    
+
     file_path.write_bytes(response.content)
     print(f"Thumbnail saved to {file_path}")
 
-def main():
+
+def main() -> None:
     logo_path = Path("src") / "images" / "icon.png"
     thumbnail_path = Path("src") / "images" / "og-thumbnail.png"
 
@@ -30,10 +33,11 @@ def main():
     thumbnail_path.parent.mkdir(parents=True, exist_ok=True)
 
     random_seed = secrets.token_hex(16)
-    
+
     # Generate logo and thumbnail
     generate_random_logo(random_seed, logo_path)
     generate_random_thumbnail(1200, 800, thumbnail_path)
+
 
 if __name__ == "__main__":
     main()
